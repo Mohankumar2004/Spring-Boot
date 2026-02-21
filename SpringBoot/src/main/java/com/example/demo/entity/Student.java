541e33c45d13e00c54ae0e0d693ca55d3921ed6e
Mohankumar <Mohankumar2004@users.noreply.github.com>
2026-02-21 15:33:48 +0530

    21-2-26

---
+++ SpringBoot/src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Student {
+public Student() {
+	// TODO Auto-generated constructor stub
+	System.out.println("Student created");
+}
+
+	@Id
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
