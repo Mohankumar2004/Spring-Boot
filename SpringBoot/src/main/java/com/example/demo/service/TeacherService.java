bcfd4cb49bc4b9f366d22643e40691f67ad886ff
Mohankumar <Mohankumar2004@users.noreply.github.com>
2026-03-04 17:44:56 +0530

    4-3-26

--- SpringBoot/src/main/java/com/example/demo/service/TeacherService.java
+++ SpringBoot/src/main/java/com/example/demo/service/TeacherService.java
@@ -1,5 +1,19 @@
 package com.example.demo.service;
 
-public interface TeacherService {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Teacher;
+
+public interface TeacherService {
+	public String home();
+	public List<Teacher> getAllTeacher();
+	public Teacher getById(@RequestParam int id);
+	public Teacher getByIdName(@PathVariable int id, @PathVariable String name);
+	public String create();
+	public Teacher save(@RequestBody Teacher teacher);
+	
 }
--- SpringBoot/src/main/java/com/example/demo/service/TeacherServiceImp.java
+++ SpringBoot/src/main/java/com/example/demo/service/TeacherServiceImp.java
@@ -1,5 +1,45 @@
 package com.example.demo.service;
 
-public class TeacherServiceImp {
+import java.util.List;
+
+import com.example.demo.entity.Teacher;
+
+public class TeacherServiceImp implements TeacherService {
+
+	@Override
+	public String home() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Teacher> getAllTeacher() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Teacher getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Teacher getByIdName(int id, String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Teacher save(Teacher teacher) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
